b3711989fdf3c8d3c9bd886356fb884b8fc4f638
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-01-16 09:35:15 +0900

    28_Find_the_Index_of_the_First_Occurrence_in_a_String

---
+++ LeetCode75/28_Find_the_Index_of_the_First_Occurrence_in_a_String/28_Find_the_Index_of_the_First_Occurrence_in_a_String.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        val searchStr = haystack.indexOf(needle)
+        return searchStr
+    }
+}
+
+/*
+・indexOfを使用して、文字列検索？
+ */
