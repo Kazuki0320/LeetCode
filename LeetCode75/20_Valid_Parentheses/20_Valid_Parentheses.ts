f007e517ef1534ea79ba5cd1e4e5b274ef897a98
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-02-02 15:28:39 +0900

    20_Valid_Parentheses

---
+++ LeetCode75/20_Valid_Parentheses/20_Valid_Parentheses.ts
@@ -0,0 +1,30 @@
+function isValid(s: string): boolean {
+    const map = new Map<string, string>([
+        ["]", "["],
+        [")", "("],
+        ["}", "{"]
+    ]);
+
+    const stack: string[] = [];
+
+    for (const char of s) {
+        if (map.has(char)) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+
+            if (map.get(char) !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+};
+
+/**
+・mapで、keyが閉じ括弧で、valueが開き括弧で、変数宣言
+・
+
+<わからないこと>
+    - valueが等しい場合、keyでの比較ができるのかわからない
+ */
