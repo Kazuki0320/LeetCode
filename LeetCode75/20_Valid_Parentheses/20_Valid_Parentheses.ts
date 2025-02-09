7f9268d0cfc3b03ff429a44fb065f099039aa815
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-02-09 20:26:48 +0900

    20_Valid_Parentheses.ts

--- LeetCode75/20_Valid_Parentheses/20_Valid_Parentheses.ts
+++ LeetCode75/20_Valid_Parentheses/20_Valid_Parentheses.ts
@@ -1,15 +1,14 @@
 function isValid(s: string): boolean {
-    const map = new Map<string, string>([
-        ["]", "["],
-        [")", "("],
-        ["}", "{"]
+    const map = new Map<string, string> ([
+        [')', '('],
+        ['}', '{'],
+        [']', '['],
     ]);
-
     const stack: string[] = [];
 
-    for (const char of s) {
+    for (let char of s) {
         if (map.has(char)) {
-            const topElement = stack.length > 0 ? stack.pop() : '#';
+            const topElement = stack.length > 0 ? stack.pop() : "#";
 
             if (map.get(char) !== topElement) {
                 return false;
@@ -18,11 +17,21 @@ function isValid(s: string): boolean {
             stack.push(char);
         }
     }
+
     return stack.length === 0;
-};
+}
+
+const str = "[]";
+console.log(isValid(str));
 
 /**
-
-<わからないこと>
-    - valueが等しい場合、keyでの比較ができるのかわからない
+ 【実装】
+ 1.閉じ括弧と開き括弧のkeyとvalueを持つ変数を宣言
+ 2.与えられてきた開き括弧の文字列の配列を宣言する
+ 3.sの配列の要素をループ処理
+ 4.もし、map変数にキーが存在する場合
+    - stackの最後の値を取得
+    - もし、mapのキー値と等しくない場合
+        - falseを返す
+ 5.もし、map変数にキーが存在しない場合
  */
