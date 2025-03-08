7a8e810c4ad45a55287b221d88a3b8c5b60f7c0f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-08 16:36:37 +0900

    242_Valid_Anagram correct

--- LeetCode75/242_Valid_Anagram/242_Valid_Anagram.ts
+++ LeetCode75/242_Valid_Anagram/242_Valid_Anagram.ts
@@ -5,8 +5,11 @@ function isAnagram(s: string, t: string): boolean {
     if (sLen !== tLen) return false
 
     for (let i = 0; i < sLen; i++) {
-        if (!t.includes(s[i])) return false
+        if (t.includes(s[i])) {
+            t = t.replace(s[i], '')
+        }
     }
+    if (t.length > 0) return false
     return true
 }
 
