417aae5b00a49ab264e47b20d1098f79d78f2f97
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-13 00:06:11 +0900

    Other Approach 344. Reverse String

--- String/344_Reverse_String/344_Reverse_String.ts
+++ String/344_Reverse_String/344_Reverse_String.ts
@@ -1,6 +1,14 @@
-/**
- Do not return anything, modify s in-place instead.
- */
 function reverseString(s: string[]): void {
-    s.reverse()
-};
+    let left = 0, right = s.length - 1;
+    
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]]; // 交換
+        left++;
+        right--;
+    }
+}
+
+const s = ["h", "e", "l", "l", "o"];
+reverseString(s); // ここでは戻り値なし
+console.log(s); // ["o", "l", "l", "e", "h"]
+
