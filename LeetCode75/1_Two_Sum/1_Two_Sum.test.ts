cbe1d142083194938a0566d4fdd0dcd7661f3f00
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-05-03 23:56:28 +0900

    各テストを一つのケースにまとめる

--- LeetCode75/1_Two_Sum/1_Two_Sum.test.ts
+++ LeetCode75/1_Two_Sum/1_Two_Sum.test.ts
@@ -2,27 +2,14 @@ import { twoSum } from './1_Two_Sum_ver2';
 
 describe('twoSum', () => {
 	test('基本的なケース1', () => {
-		const nums = [1, 2, 3];
-		const target = 3;
-		const result = twoSum(nums, target);
-		expect(result).toEqual([0, 1]);
+		expect(twoSum([1, 2, 3], 3)).toEqual([0, 1]);
 	})
-})
 
-describe('twoSum', () => {
 	test('基本的なケース2', () => {
-		const nums = [2, 7, 11, 15];
-		const target = 9;
-		const result = twoSum(nums, target);
-		expect(result).toEqual([0,1]);
+		expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
 	})
-})
 
-describe('twoSum', () => {
 	test('空の配列', () => {
-		const nums: number[] = [];
-		const target = 0;
-		const result = twoSum(nums, target);
-		expect(result).toEqual([]);
+		expect(twoSum([], 0)).toEqual([]);
 	})
 })
