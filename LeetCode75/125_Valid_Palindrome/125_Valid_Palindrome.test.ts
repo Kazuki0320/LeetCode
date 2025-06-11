cad2f0c4c4a6576be94f0418572b47ed8554b236
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-06-11 09:15:51 +0900

    isPalindrome　関数作成

---
+++ LeetCode75/125_Valid_Palindrome/125_Valid_Palindrome.test.ts
@@ -0,0 +1,9 @@
+import { isPalindrome } from "./125_Valid_Palindrome";
+
+describe('isPalindrome', () => {
+	test('基本的なケース', () => {
+		const s = "A man, a plan, a canal: Panama";
+		const result = isPalindrome(s);
+		expect(result).toEqual('amanaplanacanalpanama');
+	})
+})
---
+++ LeetCode75/125_Valid_Palindrome/125_Valid_Palindrome.ts
@@ -0,0 +1,5 @@
+export function isPalindrome(s: string): boolean {
+	// 空白を削除する
+	// 大文字を小文字にする
+	
+}
