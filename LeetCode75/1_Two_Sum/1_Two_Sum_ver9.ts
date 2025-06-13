1a32927545a4f54630568d11bbed5133791e1e14
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-06-13 21:42:13 +0900

    TwoSum1　パターン2

--- LeetCode75/1_Two_Sum/1_Two_Sum_ver9.ts
+++ LeetCode75/1_Two_Sum/1_Two_Sum_ver9.ts
@@ -1,11 +1,13 @@
 export function twoSum(nums: number[], target: number): number[] {
-	const len = nums.length;
+	const map = new Map<number, number>();
 
-	for (let i = 0; i < len; i++) {
-		for (let j = i + 1; j < len; j++) {
-			if (target === nums[i]+nums[j]) {
-				return [i, j];
-			}
+	for (let i = 0; i < nums.length; i++) {
+		const complement = target - nums[i]
+
+		if (map.has(complement)) {
+			return [map.get(complement)!, i];
+		} else {
+			map.set(nums[i], i);
 		}
 	}
 	return [];
