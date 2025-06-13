6f29e99233749c99a4dfd69e6d99aa7a9c2647a4
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-06-13 21:33:01 +0900

    TwoSum パターン1

---
+++ LeetCode75/1_Two_Sum/1_Two_Sum_ver9.ts
@@ -0,0 +1,12 @@
+export function twoSum(nums: number[], target: number): number[] {
+	const len = nums.length;
+
+	for (let i = 0; i < len; i++) {
+		for (let j = i + 1; j < len; j++) {
+			if (target === nums[i]+nums[j]) {
+				return [i, j];
+			}
+		}
+	}
+	return [];
+}
