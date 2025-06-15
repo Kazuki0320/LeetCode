77357f6e27aa9b936237a9c391b047f42cbc81bd
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-06-15 23:18:10 +0900

    3Sum 自作コード完成

--- LeetCode75/15_3Sum/15_3Sum_ver4.ts
+++ LeetCode75/15_3Sum/15_3Sum_ver4.ts
@@ -1,27 +1,29 @@
-export function threeSum(nums: number[]): number[][]{
+export function threeSum(nums: number[]): number[][] {
 	const len = nums.length;
-	const result: number[][] = [];
+	const result:number[][] = [];
 
 	if (len < 3) return [];
 
 	nums.sort((a, b) => a - b);
 
-	for (let i = 0; i < len - 2; i++) {
+	for (let i = 0;i < len; i++) {
+		if (i > 0 && nums[i] === nums[i-1]) continue;
+
 		let left = i + 1;
 		let right = len - 1;
 
-		if (i > 0 && nums[i] === nums[i - 1]) continue;
+		while(left < right) {
+			const sum = nums[i] + nums[left] + nums[right];
 
-		while (left < right) {
-			
-			if (0 === nums[i] + nums[left] + nums[right]) {
+			if (sum === 0) {
 				result.push([nums[i], nums[left], nums[right]]);
-				while (left < right && nums[left] === nums[left+1]) left++;
-				while (left < right && nums[right] === nums[right-1]) right--;
+
+				while (left < right && nums[left] === nums[left - 1]) left++;
+				while (left < right && nums[right] === nums[right - 1]) right--;
 
 				left++;
 				right--;
-			} else if (nums[i] + nums[left] + nums[right] < 0) {
+			} else if (sum < 0) {
 				left++;
 			} else {
 				right--;
