b7926822b78c8eb381ba72592f96d72d9ef9ab0b
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-07-13 21:49:12 +0900

    3. Longest Substring Without Repeating Characters 修正ver

--- LeetCode75/3_Longest_Substring_Without_Repeating_Characters/3_Longest_Substring_Without_Repeating_Characters_ver2.ts
+++ LeetCode75/3_Longest_Substring_Without_Repeating_Characters/3_Longest_Substring_Without_Repeating_Characters_ver2.ts
@@ -1,17 +1,16 @@
 export function lengthOfLongestSubstring(s:string): number {
 	const map = new Map<string, number>();
 	let left = 0;
-	let right = 0;
 	let maxLen = 0;
 
-	for (let i = 0; i < s.length; i++) {
-		const char = s[i];
+	for (let right = 0; right < s.length; right++) {
+		const char = s[right];
 
-		if (map.has(char)) {
+		if (map.has(char) && left <= map.get(char)!) {
 			left = map.get(char)! + 1;
 		}
 
-		map.set(char, i);
+		map.set(char, right);
 		maxLen = Math.max(maxLen, right-left+1);
 	}
 	return maxLen
@@ -28,5 +27,7 @@ export function lengthOfLongestSubstring(s:string): number {
  *  - right:部分文字列の終わり
  * ・ 最後に出現した文字列の場所を格納するためのmap
  * ・ maxLenを求める際は、right-left+1と前回のmaxLenを比較する
+ * ・ 文字が重複して出現した場合、leftを「前回出現した場所＋１」にジャンプする
+ * ・ ただし、leftは常に前回の位置以上である必要がある（=過去に戻らない）
  */
 
