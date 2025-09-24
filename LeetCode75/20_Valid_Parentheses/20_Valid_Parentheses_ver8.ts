f736206f9cb649c343a6fcde017574db5eb7aece
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-09-24 22:28:39 +0900

    20_Valid_Parentheses_ver8.ts ノーマル完了

---
+++ LeetCode75/20_Valid_Parentheses/20_Valid_Parentheses_ver8.ts
@@ -0,0 +1,23 @@
+function validParentheses(s: string): boolean {
+	const map = new Map([
+		[")", "("],
+		["}", "{"],
+		["]", "["]
+	])
+
+	const stack: string[] = []
+
+	for (let char of s) {
+		if (map.has(char)) {
+			const needElement = map.get(char)
+			const topElement = stack.pop()
+
+			if (needElement !== topElement) {
+				return false
+			}
+		} else {
+			stack.push(char)
+		}
+	}
+	return stack.length === 0;
+}
