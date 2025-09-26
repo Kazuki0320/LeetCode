c6b5360887f61766459e46e445a8511228535c8a
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-09-26 19:33:32 +0900

    15_3Sum_ver8.ts どこが間違っているのか分からぬ

--- LeetCode75/15_3Sum/15_3Sum_ver8.ts
+++ LeetCode75/15_3Sum/15_3Sum_ver8.ts
@@ -9,19 +9,22 @@ function threeSum(nums:number[]): number[][] {
 		let left = i+1
 		let right = len - 1
 
-		let sum = nums[i] + nums[left] + nums[right]
-		if (sum === 0) {
-			result.push([nums[i], nums[left], nums[right]])
-
-			while (left < right && nums[left] === nums[left+1]) left++;
-			while (left < right && nums[right] === nums[right-1]) right ++
-
-			left++;
-			right--;
-		} else if (sum < 0){
-			left++
-		} else {
-			right--
+		// Two-Pointerを利用するのに、leftからrightまでのループが抜けていた
+		while (left<right) {
+			let sum = nums[i] + nums[left] + nums[right]
+			if (sum === 0) {
+				result.push([nums[i], nums[left], nums[right]])
+	
+				while (left < right && nums[left] === nums[left+1]) left++;
+				while (left < right && nums[right] === nums[right-1]) right ++
+	
+				left++;
+				right--;
+			} else if (sum < 0){
+				left++
+			} else {
+				right--
+			}
 		}
 	}
 	return result
