45f82e90c62e8933956f4ede8befa86e50657c6f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-09-30 21:51:05 +0900

    155_Min_Stack_ver3.ts 2つのスタック方式

---
+++ LeetCode75/155_Min_Stack/155_Min_Stack_ver3.ts
@@ -0,0 +1,31 @@
+class MinStack {
+	private dataStack: number[] = [];
+	private minStack: number[] = [];
+
+	constructor() {}
+
+	push(val: number): void {
+		this.dataStack.push(val)
+		// minStackが空ならvalを積む
+		// 空でなければ「直前の最小値」と比較して小さい方を積む
+		if (this.minStack.length === 0) {
+			this.minStack.push(val)
+		} else {
+			const currentMin = this.minStack[this.minStack.length - 1];
+			this.minStack.push(Math.min(val, currentMin));
+		}
+	}
+
+	pop(): void {
+		this.dataStack.pop()
+		this.minStack.pop()
+	}
+
+	top():number {
+		return this.dataStack[this.dataStack.length-1]
+	}
+
+	getMin(): number {
+		return this.minStack[this.minStack.length-1];
+	}
+}
