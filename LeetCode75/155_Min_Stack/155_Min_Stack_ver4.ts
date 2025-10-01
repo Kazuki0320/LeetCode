73c14a0cdf62a1b8741fef440b955fa29bddbecd
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-10-01 22:42:20 +0900

    155_Min_Stack_ver4.ts 処理追加

--- LeetCode75/155_Min_Stack/155_Min_Stack_ver4.ts
+++ LeetCode75/155_Min_Stack/155_Min_Stack_ver4.ts
@@ -1,34 +1,34 @@
-class MinStack {
-	private dataStack: number[] = [];
-	private minStack: number [] = [];
+export class MinStack {
+    private dataStack: number[] = [];
+    private minStack: number[] = [];
 
-	constructor() {} 
+    constructor() {} 
 
-	push(val :number): void {
-		this.dataStack.push(val);
+    push(val: number): void {
+        this.dataStack.push(val);
 
-		// minStackが空ならvalを積む
-		// 空でなければ、「直前の最小値」と比較して小さい方を積む
-		if (this.minStack.length === 0) {
-			this.minStack.push(val);
-		} else {
-			const currentMin = this.minStack[this.minStack.length - 1];
-			this.minStack.push(Math.min(val, currentMin))
-		}
-	}
+        // minStackが空ならvalを積む
+        // 空でなければ、「直前の最小値」と比較して小さい方を積む
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+        } else {
+            const currentMin = this.minStack[this.minStack.length - 1];
+            this.minStack.push(Math.min(val, currentMin));
+        }
+    }
 
-	pop(): void {
-		this.dataStack.pop();
-		this.minStack.pop();
-	}
+    pop(): void {
+        this.dataStack.pop();
+        this.minStack.pop();
+    }
 
-	top(): number {
-		return this.dataStack[this.dataStack.length-1]
-	}
+    top(): number {
+        return this.dataStack[this.dataStack.length - 1];
+    }
 
-	getMin(): number {
-		return this.minStack[this.minStack.length-1];
-	}
+    getMin(): number {
+        return this.minStack.length === 0 ? 0 : this.minStack[this.minStack.length - 1];
+    }
 }
 
 /**
@@ -48,6 +48,6 @@ class MinStack {
  *  - this.dataStack[this.dataStack.length-1]
  * 
  * - getMin
- *   - minStackに積まれている最後の要素を取得
- *   - this.minStack[this.minStack.length-1]
+ *   - minStackが空の場合は0を返す
+ *   - そうでない場合は、minStackの最後の要素を取得
  */
