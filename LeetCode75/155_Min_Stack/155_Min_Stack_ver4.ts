2c7eb94dde45c718b9e376c1deb247efeb9cfe1f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-10-01 22:42:06 +0900

    155_Min_Stack_ver4.ts からの配列に関する処理が抜けている

---
+++ LeetCode75/155_Min_Stack/155_Min_Stack_ver4.ts
@@ -0,0 +1,53 @@
+class MinStack {
+	private dataStack: number[] = [];
+	private minStack: number [] = [];
+
+	constructor() {} 
+
+	push(val :number): void {
+		this.dataStack.push(val);
+
+		// minStackが空ならvalを積む
+		// 空でなければ、「直前の最小値」と比較して小さい方を積む
+		if (this.minStack.length === 0) {
+			this.minStack.push(val);
+		} else {
+			const currentMin = this.minStack[this.minStack.length - 1];
+			this.minStack.push(Math.min(val, currentMin))
+		}
+	}
+
+	pop(): void {
+		this.dataStack.pop();
+		this.minStack.pop();
+	}
+
+	top(): number {
+		return this.dataStack[this.dataStack.length-1]
+	}
+
+	getMin(): number {
+		return this.minStack[this.minStack.length-1];
+	}
+}
+
+/**
+ * それぞれの関数には役割がある
+ * - push
+ *  - dataStackに値を積む
+ *  - minStackは、条件を分ける
+ *   - 空の場合は、その値が最小値となる
+ *   - 空ではない場合は、直前の値と比較して小さい値を積む
+ * 
+ * - pop
+ *   - dataStackの末尾の要素を除く
+ *   - minStackの末尾の要素を除く
+ * 
+ * - top
+ *  - dataStackの末尾の要素を取得
+ *  - this.dataStack[this.dataStack.length-1]
+ * 
+ * - getMin
+ *   - minStackに積まれている最後の要素を取得
+ *   - this.minStack[this.minStack.length-1]
+ */
