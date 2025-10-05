5f2c227a34e4b4aa97353a29bf94efde89f9f968
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-10-05 22:38:43 +0900

    438_Find_All_Anagrams_in_a_String_ver2.ts  処理追加

--- LeetCode75/438_Find_All_Anagrams_in_a_String/438_Find_All_Anagrams_in_a_String_ver2.ts
+++ LeetCode75/438_Find_All_Anagrams_in_a_String/438_Find_All_Anagrams_in_a_String_ver2.ts
@@ -23,3 +23,54 @@
  *  - もし、等しい時はresultに結果をpush
  * 
  */
+function mapsAreEqual(a: Map<string, number>, b:  Map<string, number>): boolean {
+	if (a.size !== b.size) return false;
+	for (const [key, val] of a) {
+		if (b.get(key) !== val) return false;
+	}
+
+	return true;
+}
+
+
+function findAnagrams(s: string, p: string): number[] {
+	const pMap = new Map<string, number>();
+	const sMap = new Map<string, number>();
+	const result: number[] = [];
+
+	// pの文字頻度を記録
+	for (const char of p) {
+		const count = pMap.get(char) || 0;
+		pMap.set(char, count+1);
+	}
+
+	const pLen = p.length;
+
+	for (let i = 0; i < s.length; i++) {
+		// 1.s[i] をsMapに追加
+		const inChar = s[i];
+
+		sMap.set(inChar, (sMap.get(inChar) || 0) + 1);
+
+		// 2.ウィンドウサイズを超えたら、左端の文字を除外
+		if (i >= pLen) {
+			// 現在のウィンドウから外れる文字を取得
+			const outChar = s[i - pLen];
+			// その文字のカウントを1減らす
+			const count = sMap.get(outChar)! - 1;
+
+			// カウントが0なら削除、そうでなければ更新
+			if (count === 0) {
+				sMap.delete(outChar);
+			} else {
+				sMap.set(outChar, count);
+			}
+		}
+
+		// 3.pと同じサイズのウィンドウに達したら比較
+		if (i >= pLen - 1 && mapsAreEqual(sMap, pMap)) {
+			result.push(i - pLen + 1);
+		}
+	}
+	return result;
+}
