1c3e77ecf083fae83aab57155d3be77eff21d506
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-10-09 21:19:02 +0900

    206_Reverse_Linked_List_ver5.ts 言語化処理

---
+++ LeetCode75/206_Reverse_Linked_List/206_Reverse_Linked_List_ver5.ts
@@ -0,0 +1,25 @@
+export function reverseList(head: ListNode | null): ListNode | null {
+	/**
+	 * 例 [1,3,5]
+	 * 
+	 * 目的
+	 * リストを逆順にする
+	 * 
+	 * 処理
+	 * すでに処理したノード
+	 * - null
+	 * 現在のノード
+	 * - head
+	 * 
+	 * currentがnullになるまでループ処理
+	 * - current.nextを退避
+	 *  - current.next以降がnextに追加される
+	 *  - next = 3→5→null
+	 *  - current = 1→3→5
+	 * - 退避用のnext
+	 * - すでに処理したノードの前に、今回のリストノードを追加
+	 *  - current.nextに、既に処理したノードを更新
+	 * - 追加されたノードを既に処理されたノードに更新
+	 * - currentを次のリストに進める
+	 */
+}
