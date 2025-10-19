5384081be0b0105f82596c34ba1ac3144b115c19
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-10-19 13:41:03 +0900

    155_Min_Stack_ver9.ts 処理の目的を言語化

---
+++ LeetCode75/155_Min_Stack/155_Min_Stack_ver9.ts
@@ -0,0 +1,21 @@
+/**
+ * 目的
+ * MinStackクラスから、関数を呼びして適切な値を返す
+ * 
+ * 処理
+ * dataStack:与えられたvalを積む箱
+ * minStack:minの値のみを扱う箱
+ * 
+ * push 
+ *  - dataStackの箱につむ
+ *  - minStackに積まれている既存のものと比較
+ *   - minStackに箱をつむ
+ * pop
+ *  - dataStackとminStackに最後に積まれた値を取得
+ * top
+ *  - dataStackの一番最後の値を取得
+ * getMin
+ *  - minStackを取得
+ *   - もし、minStackが0の場合は、0を返す
+ */
+
