abe977a0a7d700ba7d6e18ae5e4430a1e007218f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-10-25 16:00:16 +0900

    438_Find_All_Anagrams_in_a_String_ver8.ts 模写実装

--- LeetCode75/438_Find_All_Anagrams_in_a_String/438_Find_All_Anagrams_in_a_String_ver8.ts
+++ LeetCode75/438_Find_All_Anagrams_in_a_String/438_Find_All_Anagrams_in_a_String_ver8.ts
@@ -13,6 +13,15 @@
  *  - aとbのサイズが等しいか
  * 
  */
+
+function mapsAreEqual(a: Map<string, number>, b: Map<string, number>) {
+	if (a.size !== b.size) return false
+	for (let [key, val] of a) {
+		if (b.get(key) !== val) return false
+	}
+	return true
+}
+
 export function findAnagrams(s: string, p: string): number[] {
 	const pMap = new Map<string, number>();
 	const sMap = new Map<string, number>();
@@ -20,18 +29,30 @@ export function findAnagrams(s: string, p: string): number[] {
 	const result: number[] = []
 
 	for (let char of p) {
-
+		const count = pMap.get(char) || 0
+		pMap.set(char, count+1)
 	}
 
+	const pLen = p.length;
+
 	for (let i = 0; i < s.length; i++) {
+		const inChar = s[i]
+
+		sMap.set(inChar, (sMap.get(inChar) || 0) + 1)
 
 		if (i >= pLen) {
-			if (count === 0) {
+			const outChar = s[i-pLen]
 
+			const count = sMap.get(outChar)! - 1
+			if (count === 0) {
+				sMap.delete(outChar)
+			} else {
+				sMap.set(outChar, count)
 			}
 		}
 		if (i >= pLen - 1 && mapsAreEqual(sMap, pMap)) {
-			
+			result.push(i-pLen+1)
 		}
 	}
+	return result;
 }
