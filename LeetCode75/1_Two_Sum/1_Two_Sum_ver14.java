a198273aaafab8189a5413a44b1cf2ce5a94e115
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-11-02 11:53:56 +0900

    1_Two_Sum_ver14.java java実装

--- LeetCode75/1_Two_Sum/1_Two_Sum_ver14.java
+++ LeetCode75/1_Two_Sum/1_Two_Sum_ver14.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * [2,7,11,15]
  * 目的
@@ -12,7 +15,25 @@
  * - もし、mapに保存されていない場合
  *  - mapに、keyとvalueを保存する
  *  - keyに何を保存するのか
- *   - target-現在の要素=key
+ *   - 現在の要素
  *  - valueに何を保存するのか
  *   - その数値の要素数
  */
+
+class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (var i = 0; i < nums.length; i++) {
+			int current = nums[i];
+			int comp = target-current;
+
+			if (map.containsKey(comp)) {
+				return new int[] { map.get(comp), i };
+			} else {
+				map.put(current, i);
+			}
+ 		}
+		return new int[0];
+	}
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    checkstyle
 }
 
 repositories {
@@ -19,6 +20,35 @@ application {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// LeetCode の問題配下 (LeetCode75/**) に置いた .java もコンパイル対象にする
+sourceSets {
+    create("problems") {
+        java.setSrcDirs(listOf("LeetCode75"))
+    }
+}
+
+// 問題用の Java をビルドしてエラー検出 (-Xlint を有効化)
+tasks.register<JavaCompile>("compileProblems") {
+    source = sourceSets["problems"].java
+    classpath = files() // 依存なし
+    destinationDirectory.set(layout.buildDirectory.dir("classes/problems"))
+    options.release.set(17)
+    options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:deprecation", "-Xlint:unchecked"))
+}
+
+// `gradle check` でエラーチェックが走るように
+tasks.named("check").configure {
+    dependsOn("compileJava")
+    dependsOn("compileProblems")
+    dependsOn("checkstyleMain")
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
 }
 
 
