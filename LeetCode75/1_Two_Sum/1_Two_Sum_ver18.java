355660c7ef263ca30f5ef091ad36aa5f1ae3a88f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-11-07 22:39:01 +0900

    1_Two_Sum_ver18 正解

---
+++ LeetCode75/1_Two_Sum/1_Two_Sum_ver18.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class twoSumVer18 {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (var i = 0; i < nums.length; i++) {
+			int current = nums[i];
+			int comp = target - current;
+
+			if (map.containsKey(comp)) {
+				return new int[] { map.get(comp), i };
+			} else {
+				map.put(nums[i], i);
+			}
+		}
+		return new int[0];
+	}
+}
