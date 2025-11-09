a088fb49d9c511457dd67c269f967278e16d5931
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-11-09 16:11:45 +0900

    1_Two_Sum_ver21.java Refactering

--- LeetCode75/1_Two_Sum/1_Two_Sum_ver21.java
+++ LeetCode75/1_Two_Sum/1_Two_Sum_ver21.java
@@ -2,8 +2,9 @@ class TwoSumVer21 {
 	public int[] twoSumVer21(int[] nums, int target) {
 		int numLen = nums.length;
 		for (int i = 0; i < numLen; i++) {
+			int comp = target - nums[i];
 			for (int j = i + 1; j < numLen; j++) {
-				if (nums[j] == target - nums[i]) {
+				if (nums[j] == comp) {
 					return new int[] { i, j };
 				}
 			}
