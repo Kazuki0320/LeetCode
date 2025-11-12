81193d54bf263b23eb14b282126e9aea6bacad7b
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-11-12 23:22:37 +0900

    3_Longest_Substring_Without_Repeating_Characters_ver10.java CallActive 実装

---
+++ LeetCode75/3_Longest_Substring_Without_Repeating_Characters/3_Longest_Substring_Without_Repeating_Characters_ver10.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class longestSubstringWithoutRepeatingCharactersVer10 {
+	public int lenghtOfLongestSubstring(String s) {
+		Map<Character, Integer> map = new HashMap<>();
+		int maxLen = 0, left = 0;
+
+		for (int right = 0; right < s.length(); right++) {
+		 char c = s.charAt();
+
+		 if (map.containsKey(c) && left <= map.get(c)) {
+			left = map.get(c) + 1;
+		 }
+
+		 map.put(c, right);
+
+		 maxLen = Math.max(maxLen, right-left+1);
+		}
+		return maxLen;
+	}
+}
