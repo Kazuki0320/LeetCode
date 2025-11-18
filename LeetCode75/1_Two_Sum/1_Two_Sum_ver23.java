c8b174e706faa670aa4b6a3c6b661699fa407f0d
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-11-18 23:39:57 +0900

    1_Two_Sum_ver23 完了

---
+++ LeetCode75/1_Two_Sum/1_Two_Sum_ver23.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class twoSum23 {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int current = nums[i];
+			int comp = target - current;
+
+			if (map.containsKey(comp)) {
+				return new int[] { map.get(comp), i };
+			} else {
+				map.put(current, i);
+			}	
+		}
+		return new int[0];
+	}
+}
